d9353fa5fcfa54c479fb4bd7ee45e39d4043e947
r3tr0m <100650453+r3tr0m@users.noreply.github.com>
2022-08-01 16:26:05 -0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -32,44 +32,43 @@ inst_t* lexar(char *line){
 	while( (line[inc] != '\x0a') || (line[inc] == ';') )
 	{
 
-		//printf("-> %c\n",line[inc]);
 
 		for(uint8_t byte=0; byte < 18; byte++)
 		{
 
 			if( (line[inc] == '%') || (line[inc] == '$') || (line[inc] == '@') ){
-				//printf("sim < - \n");
+				
 				sims = true;
 				out  = false;
 				break;
 
 			}else if( (line[inc] == ',') || (line[inc] == ')') ){
-
+				
 				sims = false;
 				out  = true;
-
-				//printf("fsim < - \n");
 
 				break;
 
 			}else if( (numbers[byte] == line[inc]) && (sims == true) ){
 
-				//printf("num <- num\n");
 				out = false;
 				break;
 
 			}else{
-				//printf("t-out <- ch\n");
+				
 				out = true;
+				
 			}
 
 		}
 
-		if(out == true){
+		if(out == true)
+		{
 			code[inc] = line[inc];
 
-		}else if( (out == false) && (sims == true) ){
-
+		}else if( (out == false) && (sims == true) )
+		{
+	
 			cnumber[cinc] = line[inc];
 			cinc++;
 
@@ -130,32 +129,22 @@ bin_t* assembler(asm_t *program){
 
 		//AUTOMATA
 
-		printf("[+] LEXAR -> %u\n",ibyte);
-
 		inst_t	*inst 		= lexar(&program->file_code[ibyte]);
 
-		if(inst->cnum[0] != 0x00){
+		if(inst->cnum[0] != 0x00)
+		{
 			int16_t num 	= convert(inst->cnum);
 			inst->number	= num;
 		}
-
-		printf("[+]Hash\n");
 
 		uint32_t hcode		= hash(inst->filecode);
 		inst->hash		= hcode;
 
 		//PUT IN MEMORY
 
-		printf("[+]Opcode\n");
 
 		inc += opcode(&binary[inc],inst);
 		ibyte += inst->ibyte +1;
-
-		printf("%u\n",inc);
-
-		printf("FCode: %s\n",inst->filecode);
-		printf("Num:   %hu\n",inst->number);
-		printf("Hash:  %u\n",inst->hash);
 
 		free(inst);
 
@@ -172,7 +161,8 @@ int main(uint8_t argc,char **argv){
 
 	//MAIN ENTRY
 
-	if(argc < 2){
+	if(argc < 2)
+	{
 		printf("%s <program.asm>\n",argv[0]);
 		return -1;
 	}
